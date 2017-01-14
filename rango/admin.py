4f6ccda70b452481af14a02eb680a9730c49c2af
Malion Hoxhallari <malioni91@gmail.com>
2017-01-14 17:08:39 +0000

    Finished Ch.5

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from django.contrib.admin import AdminSite
+
+
+# add category and url as columsn in Admin interface
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
+
 
 
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
+
