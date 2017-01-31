7046363c11f865f4480103a0248e703e6ef721a9
Malion Hoxhallari <malioni91@gmail.com>
2017-01-31 12:16:08 +0000

    Fixed populate script

--- populate_rango.py
+++ populate_rango.py
@@ -43,8 +43,8 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
 
-    for cat, cat_data in cats.iteritems():
-        c = add_cat(cat)
+    for cat, cat_data in cats.items():
+        c = add_cat(cat, cat_data["views"], cat_data["likes"])
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"], p["views"])
 
